6049a43cb4374e3f8872a0b8fae0b85a6aab7d72
EmilianoSP9033 <AL03098431@tecmilenio.mx>
2026-09-17 19:26:46 -0600

    feat: agrega main.tf vulnerable y pipeline de github actions

---
+++ main.tf
@@ -0,0 +1,22 @@
+﻿terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4.0"
+    }
+  }
+}
+
+provider "local" {}
+
+# VULNERABILIDAD INTENCIONAL: Secreto expuesto por defecto
+variable "api_secret_token" {
+  type        = string
+  description = "Token secreto de la API"
+  default     = "SUPER_SECRET_TOKEN_EXPUESTO_12345" 
+}
+
+resource "local_file" "output_secreto" {
+  filename = "${path.module}/configuracion.txt"
+  content  = "El sistema se ha iniciado correctamente usando el token."
+}
